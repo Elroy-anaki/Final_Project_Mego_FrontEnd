c2e19fd4fde0ea3c79599b81573910492556f213
Elroy Anaki <elroy.a1912@gmail.com>
2024-12-18 20:44:53 +0200

    added order-place

--- client/src/pages/public/OrderPlace/OrderPlace.jsx
+++ client/src/pages/public/OrderPlace/OrderPlace.jsx
@@ -1,17 +1,40 @@
-import React, { useContext, useState } from 'react'
-import Select from './Select'
+// Import Hooks +  Network utils
+import { useContext, useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { OrderDetailsContext } from '../../../Contexts/OrderDetailsContext';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+// Import Context + Utils
+import { OrderDetailsContext } from '../../../Contexts/OrderDetailsContext';
+import { hours } from '../../../helpers/restaurant';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
+
+
+
+const formatDateToLocal = (date) => {
+    return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+        .toISOString()
+        .split('T')[0];
+};
+
+const initValues = {
+    date: "",
+    time: "",
+    people: ""
+}
 
 function OrderPlace() {
-    const {setOrderDetails} = useContext(OrderDetailsContext)
+    // Context + Utils
+    const { setOrderDetails, orderDetails } = useContext(OrderDetailsContext)
     const navigate = useNavigate()
-    const [values, setValues] = useState(null)
+    // Local Hooks
+    const [values, setValues] = useState(orderDetails ? orderDetails : initValues)
     const [remainingSeats, setRemainingSeats] = useState(null)
-    const [showTime, setShowTime] = useState(false)
-    const [showPeople, setShowPeople] = useState(false)
+    const [showTime, setShowTime] = useState(orderDetails ? true : false)
+    const [showPeople, setShowPeople] = useState(orderDetails ? true : false)
+    // const [date, setDate] = useState(new Date());
+
 
     async function handleChange(e) {
         const { name, value } = e.target;
@@ -27,68 +50,79 @@ function OrderPlace() {
         mutationKey: ['getRemainingSeats'],
         mutationFn: async (data) => await axios.post(`/restaurant/get-remaining-seats`, data),
         onSuccess: (data) => {
-            alert(data.data.data[0].remaining)
             const numberOfEaters = []
-            for(let i = 1; i <= data.data.data[0].remaining; i++)numberOfEaters.push(i)
+            for (let i = 1; i <= data.data.data[0].remaining; i++)numberOfEaters.push(i)
             console.log(numberOfEaters);
             setRemainingSeats(numberOfEaters)
-            console.log(data)}
+            console.log(data)
+        }
     })
 
-    // const 
+    useEffect(() => {
+        if (!values?.time) return;
+        getRemainingSeats(values)
+    }, [values?.time])
+
     return (
         <div
             className='bg-gradient-to-br from-gray-800 to-gray-700 pb-48 '>
             <h2 className='text-center text-3xl font-bold text-amber-500 py-4'>
                 Welcome to Our Restaurant!
             </h2>
+            {/* <Calendar
+                onChange={(newDate) => {
+                console.log(formatDateToLocal(newDate));
+                // handleChange()
+                // setShowTime(true)
+                
+                
+                }}
+                value={date}
+                className="rounded-lg shadow-md border border-gray-300 p-4 bg-gray-600 text-white w-1/2 text-center py-10" // מחלקות Tailwind מותאמות
+
+            /> */}
             <div
                 className='relative shadow-2xl rounded-xl h-56 py-2 w-4/5 mx-auto my-16 bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white'
             >
                 <div
                     className='flex justify-center items-center gap-14 px-6 mt-6'
                 >
-                    
+
                     <div className='w-1/4 rounded-xl border-2 border-amber-500 bg-gray-800 px-4 py-3 shadow-inner space-y-3'>
-                    <p className='text-lg'>Date</p>
+                        <p className='text-lg'>Date</p>
 
                         <input
+                            value={values?.date}
                             className="bg-gray-600 text-white w-full h-12 rounded-xl"
                             // min={new Date().toISOString().split('T')[0]}
                             onChange={(e) => {
                                 handleChange(e)
                                 setShowTime(true)
                             }}
-                            // onBlur={() => setShowTime(true)}
 
                             type="date" name="date" id="date" />
                     </div>
                     {showTime && <div className='w-1/4 rounded-xl  border-2 border-amber-500 bg-gray-800 px-4 py-3 shadow-inner space-y-3'>
                         <p className='text-lg'>Time</p>
                         <select
+                            value={values?.time}
                             className="bg-gray-600 text-white w-full h-12 rounded-xl"
                             onChange={(e) => {
                                 handleChange(e)
-                                getRemainingSeats(values)
                                 setShowPeople(true)
                             }}
                             name="time"
                             id="time">
-                            <option value="16:00-17:00">16:00-17:00</option>
-                            <option value="17:00-18:00">17:00-18:00</option>
-                            <option value="18:00-19:00">18:00-19:00</option>
-                            <option value="19:00-20:00">19:00-20:00</option>
-                            <option value="20:00-21:00">20:00-21:00</option>
-                            <option value="21:00-22:00">21:00-22:00</option>
-                            <option value="22:00-23:00">22:00-23:00</option>
-                            <option value="23:00-00:00">23:00-00:00</option>
+                            {hours.map((hour) => (<option value={hour}>{hour}</option>))}
                         </select>
-                    </div> }
+                    </div>}
                     {showPeople && <div className='w-1/4 rounded-xl border-2 border-amber-500 rounded-l-lg bg-gray-800 px-4 py-3 shadow-inner space-y-3'>
-                        <p className='text-lg'>People</p>
+                        <p className='text-lg'>People <span className='text-rose-600 ml-4 text-sm'>Max: {remainingSeats ? remainingSeats.length : null}</span></p>
 
                         <select
-                             className="bg-gray-600 text-white w-full h-12 rounded-xl"
+                            value={values?.people}
+
+                            className="bg-gray-600 text-white w-full h-12 rounded-xl"
                             onChange={(e) => {
                                 handleChange(e)
                             }}
@@ -96,30 +130,32 @@ function OrderPlace() {
                             id="people">
                             {remainingSeats && remainingSeats?.map((number) => (<option value={number}>{number} People</option>))}
                         </select>
-                    </div> }
+                    </div>}
                     <button
-                        onClick={() => { 
+                        onClick={() => {
                             setShowTime(false)
                             setShowPeople(false)
                             setRemainingSeats(null)
+                            setOrderDetails(null)
 
-                         }}
+
+                        }}
                         className='absolute bottom-2 left-2 w-28 px-6 py-2 rounded-2xl bg-gradient-to-r from-rose-700 to-rose-600 text-black text-lg font-bold hover:from-rose-600 hover:to-rose-500 shadow-xl   hover:bottom-3 right-6 transition-all transform hover:scale-105'
                     >
                         Reset
                     </button>
                     <button
-                        onClick={() => { 
+                        onClick={() => {
                             setOrderDetails(values)
                             navigate('/menu')
 
-                         }}
+                        }}
                         className='absolute bottom-2 right-2 px-6 py-2 rounded-2xl bg-gradient-to-r from-green-600 to-green-500 text-black text-lg font-bold hover:from-green-500 hover:to-green-400 shadow-xl hover:bottom-3 transition-all transform hover:scale-105'
                     >
                         Save and Continue
                     </button>
-                    
-                    
+
+
                 </div>
             </div>
         </div>
