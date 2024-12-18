7cf39e1571e1e3ae1fff58c7f5ad16bc9c35a1ee
Israel Fertig <m583288718@gmail.com>
2024-12-18 15:42:51 +0200

    added table cart

--- client/src/Contexts/TableContext.jsx
+++ client/src/Contexts/TableContext.jsx
@@ -1,24 +1,66 @@
-import { createContext } from "react";
-import { useState, } from "react";
+import { createContext, useEffect } from "react";
+import { useState } from "react";
 
+export const TableContext = createContext();
 
-export const TableContext = createContext()
+function TableProvider({ children }) {
+  const [table, setTable] = useState([]);
 
-function TableProvider({children}){
-
-    const [table, setTable] = useState()
-
-
-    const tableGlobalState = {
-        table, 
-        setTable
+  function addMealToTable(meal) {
+    if (table.length > 0) {
+      const exist = table.find((m) => m.mealId === meal._id);
+      console.log("Existing meal:", exist);
+      if (!exist) {
+        setTable([...table, { mealId: meal._id, quantity: 1 }]);
+      } else {
+        const newTable = table.map((m) => {
+          if (m.mealId === meal._id) {
+            return { mealId: m.mealId, quantity: m.quantity + 1 };
+          }
+          return m;
+        });
+        setTable(newTable);
+      }
+    } else setTable([{ mealId: meal._id, quantity: 1 }]);
+  }
+  function deleteMealFromTable(meal) {
+    const exist = table.find((m) => m.mealId === meal._id);
+    if (exist.quantity > 1) {
+      const newTable = table.map((m) => {
+        if (m.mealId === meal._id) {
+          return { mealId: m.mealId, quantity: m.quantity - 1 };
+        }
+        return m;
+      });
+      console.log(meal);
+      setTable(newTable);
+    } else {
+      const newTable = table.filter((m) => m.mealId !== meal._id);
+      setTable(newTable);
     }
+  }
 
-    return (
-        <TableContext.Provider value={tableGlobalState}>
-            {children}
-        </TableContext.Provider>
-    )
+  function clearTable() {
+    setTable([]);
+  }
+
+  useEffect(() => {
+    console.log("table", table);
+  }, [table]);
+
+  const tableGlobalState = {
+    table,
+    setTable,
+    addMealToTable,
+    deleteMealFromTable,
+    clearTable,
+  };
+
+  return (
+    <TableContext.Provider value={tableGlobalState}>
+      {children}
+    </TableContext.Provider>
+  );
 }
 
 export default TableProvider;
--- client/src/pages/public/About/About.jsx
+++ client/src/pages/public/About/About.jsx
@@ -1,42 +1,83 @@
-import React from 'react'
+import { useContext } from "react"
+
+import {TableContext} from "../../../Contexts/TableContext.jsx"
+
+// const cheifs = [
+  //     {name: "Asaf Granit", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
+  //     {name: "Moshik Rot", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
+  //     {name: "AYossi Shitrit", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
+  //     ]
+  
+  function About() {
 
 
-const cheifs = [
-    {name: "Asaf Granit", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
-    {name: "Moshik Rot", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
-    {name: "AYossi Shitrit", image:"https://images.globes.co.il/images/NewGlobes/big_image_800/2020/800x392-REDEFINE_MEAT.20201001T144014.jpg"},
-    ]
+  const {addMealToTable,deleteMealFromTable,clearTable ,table } = useContext(TableContext)
 
-function About() {
+
+
+  // useEffect(() => {
+  //   setTable([{ _id: "122", name: "moti22" }]);
+  // }, []);
   return (
-    <div
-    className=''>
-        <h2
-        className='text-5xl text-center my-5'
-        >Our Cheifs</h2>
-    <div
-    className='w-full flex flex-col gap-10 items-center justify-center'>
-        {cheifs.map((cheif) => 
+//     <div
+//     className=''>
+//         <h2
+//         className='text-5xl text-center my-5'
+//         >Our Cheifs</h2>
+//     <div
+//     className='w-full flex flex-col gap-10 items-center justify-center'>
+//         {cheifs.map((cheif) => 
 
-        <div 
-        className={`flex justify-start items-center px-32
-        w-full h-96 
-        bg-[url("${cheif.image}")]
-        bg-cover
+//         <div 
+//         className={`flex justify-start items-center px-32
+//         w-full h-96 
+//         bg-[url("${cheif.image}")]
+//         bg-cover
         
         
-         `}>
-        <p
-        className='text-black text-5xl '
-        >{cheif.name}</p></div>
+//          `}>
+//         <p
+//         className='text-black text-5xl '
+//         >{cheif.name}</p></div>
         
         
       
- )}
+//  )}
         
 
-    </div>
-    </div>
+//     </div>
+//     </div>
+
+
+<div className="flex gap-5">
+<button onClick={() => addMealToTable({ _id: "12223", name: "test meal" })}>
+    Add Meal
+</button>
+<button onClick={() => addMealToTable({ _id: "12223", name: "test meal" })}>
+    Add Meal
+</button>
+<button onClick={() => addMealToTable({ _id: "123", name: "test meal" })}>
+    Add Meal
+</button>
+<button onClick={() => addMealToTable({ _id: "123", name: "test meal" })}>
+    Add Meal
+</button>
+<button onClick={() => addMealToTable({ _id: "112223", name: "test meal" })}>
+    Add Meal
+</button>
+<button onClick={() => deleteMealFromTable({ _id: "12223" })}>
+    delete1
+</button>
+<button onClick={() => clearTable({ _id: "112223", name: "test meal" })}>
+clearTable
+</button>
+
+<div>
+    {table.map((item) => (
+        <div key={item._id}>{item.name}</div>
+    ))}
+</div>
+</div>
     
   )
 }
