27bc94e16ccc80d79192d04802ef4f10cf0b1f99
Elroy Anaki <elroy.a1912@gmail.com>
2025-03-06 14:13:18 +0200

    feat: added project description

--- client/src/pages/public/Home/Home.jsx
+++ client/src/pages/public/Home/Home.jsx
@@ -1,28 +1,62 @@
-import React, { useContext } from 'react'
-import CubeLink from './CubeLink'
-import { AuthContext } from '../../../context/AuthContext'
+import React, { useContext, useState } from "react";
+import CubeLink from "./CubeLink";
+import { AuthContext } from "../../../context/AuthContext";
+import rojectDescription from "../../../projectDescription";
+import ProjectDescription from "../../../projectDescription";
 
 const cubeLinks = [
-  {name:"Menu", link:"menu", image:"https://images.unsplash.com/photo-1710732652617-264d6f860546?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudCUyMG1lbnV8ZW58MHx8MHx8fDA%3D"},
-  {name:"Order Place", link:"order-place", image:"https://images.unsplash.com/photo-1497395217579-0d6deaff0906?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8VGFibGUlMjBpbiUyMHJlc3RhdXJuYXR8ZW58MHx8MHx8fDA%3D"},
-  {name:"Take Away", link:"menu", image:"https://images.unsplash.com/photo-1585759071429-1646f76ab8c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRha2UlMjBhd2F5fGVufDB8fDB8fHww"},
-]
+  {
+    name: "Menu",
+    link: "menu",
+    image:
+      "https://images.unsplash.com/photo-1710732652617-264d6f860546?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudCUyMG1lbnV8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Order Place",
+    link: "order-place",
+    image:
+      "https://images.unsplash.com/photo-1497395217579-0d6deaff0906?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8VGFibGUlMjBpbiUyMHJlc3RhdXJuYXR8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Take Away",
+    link: "menu",
+    image:
+      "https://images.unsplash.com/photo-1585759071429-1646f76ab8c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRha2UlMjBhd2F5fGVufDB8fDB8fHww",
+  },
+];
 
 function Home() {
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
+  const [toggleDescription, setToggleDescription] = useState(true);
+
   return (
-    <div className='mt-5 w-11/12 mx-auto flex flex-col justify-center items-center space-y-20' >
-      <div>
-      <h1
-       className='text-5xl text-center text-white'
-      >Welcome, {user? user?.userName : 'Eater'}!</h1>
-      </div>
-      <div className='flex flex-wrap w-full text-center items-center justify-center gap-5'>
-        {cubeLinks.map((cubeLink) => <CubeLink key={cubeLink.name} {...cubeLink}/>)}
-      </div>
-      
+    <div className="text-center">
+      {toggleDescription ? (
+        <ProjectDescription />
+      ) : (
+        <div className="mt-5 w-11/12 mx-auto flex flex-col justify-center items-center space-y-20">
+          <div>
+            <h1 className="text-5xl text-center text-white">
+              Welcome, {user ? user?.userName : "Eater"}!
+            </h1>
+          </div>
+          <div className="flex flex-wrap w-full text-center items-center justify-center gap-5">
+            {cubeLinks.map((cubeLink) => (
+              <CubeLink key={cubeLink.name} {...cubeLink} />
+            ))}
+          </div>
+        </div>
+      )}
+      {toggleDescription && (
+        <button
+          className=" text-2xl text-white bg-gradient-to-r from-amber-600 via-amber-500 to-amber-600 rounded-xl p-3 hover:bg-gradient-to-r hover:from-amber-500 hover:via-amber-600 hover:to-amber-500"
+          onClick={() => setToggleDescription(!toggleDescription)}
+        >
+          Let's dive into the project!
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
